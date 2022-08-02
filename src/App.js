50a24d93f9736136abdd9355b1afb9ac07f465b5
David Lopez <david.lopez@veevart.com>
2022-08-01 21:28:25 -0500

    TK_2: completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>SUSSSS</h1>
+      <Navbar/>
     </div>
   );
 }
