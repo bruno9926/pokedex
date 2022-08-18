ddc725eea3e3b84af3aa7aec2ba165ae0d6a14b0
Johan Alberto Gil <johanalbertog107@gmail.com>
2022-08-17 19:57:48 -0500

    TK-5 finished

---
+++ src/PokedexLayout/components/Light/Light.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Light.css';
+
+const Light = () => {
+
+    return (
+        <div className='j-light-item'>
+            <div className='j-light'/>
+            <div className='j-light-white'/>
+        </div> 
+    )
+}
+
+export { Light };
