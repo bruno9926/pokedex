d796de2979970d40d284d1179510b25f3cdfdd49
Johan Alberto Gil <johanalbertog107@gmail.com>
2022-08-28 20:23:23 -0500

    Tercer avance (Pre-final)

--- src/Pokeview/Pokeview.jsx
+++ src/Pokeview/Pokeview.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Pokeview.scss';
+import pokemon from '../Icons/Eevee.png';
+import Type from './components/Type';
 
 const Pokeview = () => {
 
     return (
         <div className='j-pokeview-welcome'>
-
+            <div className='j-pokeview-container'>
+                <img alt='Selected Pokemon' src={pokemon}></img>
+                <div>
+                    <Type name='Planta'/>
+                    <Type name='Luchador'/>
+                    <Type name='Planta'/>
+                    <Type name='Luchador'/>
+                    <Type name='Luchador'/>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/Pokeview/components/Type.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Type.scss';
+
+const Type = ({name}) => {
+  return (
+    <div className='j-type-grass'>{name}</div>
+  )
+}
+
+export default Type;
