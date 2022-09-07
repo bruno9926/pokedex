8f64fd2b8c7340f16d420b1e0971547be95f5f14
David Lopez <david.lopez@veevart.com>
2022-09-06 19:12:58 -0500

    TK-16: completed

--- src/Pokeview/Pokeview.jsx
+++ src/Pokeview/Pokeview.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './Pokeview.scss';
-import pokemon from '../Icons/Eevee.png';
 import Type from './components/Type';
+import { PokemonContext } from '../context/PokemonContext';
 
 const Pokeview = () => {
 
+    const { state: {
+        selectedPokemonIndex: index,
+        pokemonMap
+    } } = useContext(PokemonContext);
+
+    const [pokemon, setPokemon] = useState();
+
+    useEffect(() => {
+        if (!index || Object.keys(pokemonMap).length === 0) {
+            return;
+        }
+        if (index <= Object.keys(pokemonMap).length) {
+            setPokemon(pokemonMap[index]);
+        }
+    }, [index, pokemonMap])
+
     return (
         <div className='j-pokeview-welcome'>
-            <div className='j-pokeview-container'>
-                <img alt='Selected Pokemon' src={pokemon}></img>
-                <div className='j-pokeview-type-container'>
-                    <Type type='bug'/>
-                    <Type type='rock'/>
-                </div>
-            </div>
+            {
+                pokemon ?
+                <div className='j-pokeview-container'>
+                    <img alt='Selected Pokemon' src={pokemon.img}></img>
+                    <div className='j-pokeview-type-container'>
+                        {
+                            pokemon.types.map((type, index) => (
+                                <Type key={'type ' + index} type={type} />
+                            ))
+                        }
+                    </div>
+                </div> :
+                <h1 className='no-pokemon'>
+                    Pokemon not selected
+                </h1>
+            }
         </div>
     );
 }
 
-export {Pokeview};
+export { Pokeview };
