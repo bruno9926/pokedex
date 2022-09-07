e563d17c53310a4427847ebe9700d0c41ec76be2
tinanano19 <lauratina98@gmail.com>
2022-09-06 20:01:34 -0500

    TK-17: corrections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import "./Home.scss";
 import PokeList from "../../PokeList/PokeList";
 import Welcome from "../../Welcome/Welcome";
 import Spinner from "../../Spinner/Spinner";
-import useFetchPokemon from "../../PokeList/useFetchPokemon";
 import { PokemonContext } from "../../context/PokemonContext";
 import { Pokeview } from "../../Pokeview/Pokeview";
 
@@ -12,7 +11,6 @@ const Home = () => {
     state: { isFetching, pokemonMap },
   } = useContext(PokemonContext);
   const pokemonList = Object.values(pokemonMap);
-  useFetchPokemon();
   return (
     <div className="home-page">
       {isFetching && pokemonList.length == 0 ? <Spinner /> : <div>
