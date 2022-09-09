cd41a3dd8d8d61b7fdfa0f27d40f1771fc81cb5d
johangv <johanalbertog107@gmail.com>
2022-09-08 22:59:26 -0500

    Libreria de react router

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import { PokedexLayout } from './PokedexLayout/PokedexLayout';
 // pages
 import Home from './pages/Home/Home';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <AppProvider>
-      <PokedexLayout>
-        <div className="App bg" >
-          <Navbar></Navbar>
-          <Home />
-        </div>
-      </PokedexLayout>
+      <Router>
+        <PokedexLayout>
+          <div className="App bg" >
+            <Navbar></Navbar>
+            <Home />
+          </div>
+        </PokedexLayout>
+      </Router>
     </AppProvider>
     
   );
